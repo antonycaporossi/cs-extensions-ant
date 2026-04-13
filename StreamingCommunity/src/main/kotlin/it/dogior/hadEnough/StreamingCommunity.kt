7ee8a68c2f6803fca475fd53455bc38441f90123
Antony Caporossi <antony.caporossi@gmail.com>
2026-04-13 19:35:02 +0200

    Fix

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -50,7 +50,7 @@ class StreamingCommunity : MainAPI() {
             "X-Inertia-Version" to inertiaVersion,
             "X-Requested-With" to "XMLHttpRequest",
         ).toMutableMap()
-        val mainUrl = "https://streamingcommunityz.pet/it"
+        val mainUrl = "https://streamingcommunityz.moe/it"
         var name = "StreamingCommunity"
         val TAG = "SCommunity"
     }
