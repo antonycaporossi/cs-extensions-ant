7f3aad54e1ed4765773440eac32b6d246f7d17ff
Antony Caporossi <antony.caporossi@gmail.com>
2026-07-22 20:30:57 +0200

    Update SC

--- StreamingCommunity/build.gradle.kts
+++ StreamingCommunity/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 849
+version = 850
 
 
 cloudstream {
--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -50,7 +50,7 @@ class StreamingCommunity : MainAPI() {
             "X-Inertia-Version" to inertiaVersion,
             "X-Requested-With" to "XMLHttpRequest",
         ).toMutableMap()
-        val mainUrl = "https://streamingcommunityz.plus/it"
+        val mainUrl = "https://streamingcommunityz.sale/it"
         var name = "StreamingCommunity"
         val TAG = "SCommunity"
     }
